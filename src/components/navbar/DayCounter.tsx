19e4dfb83107fed7e32fa5a6ab6ad335386da25d
Boomber <contact@boomber.xyz>
2022-03-04 17:33:51 +0900

    No need to offset for UTC when constructing DayCounter

--- src/components/navbar/DayCounter.tsx
+++ src/components/navbar/DayCounter.tsx
@@ -2,7 +2,8 @@ import { fixedDate } from "../../lib/words"
 
 export const DayCounter = () => {
 	const now = fixedDate(new Date());
-	const start = Date.UTC(2022, 2, 1, 15) - 9 * 60 * 60 * 1000;
+	const start = Date.UTC(2022, 2, 2, 0) - 9 * 60 * 60 * 1000;
+
 	const day = Math.floor((now - start) / (1000 * 60 * 60 * 24));
 
 	return <span className="text-white bg-orange-500 p-1 rounded-md">Day {day}</span>
