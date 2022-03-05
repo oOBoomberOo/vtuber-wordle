fb0af3bd9fcaec543b2f7e158082f4ae9ffc0514
Boomber <contact@boomber.xyz>
2022-03-05 15:26:49 +0900

    incorrect timezone calculation in DayCounter

--- src/components/navbar/DayCounter.tsx
+++ src/components/navbar/DayCounter.tsx
@@ -2,7 +2,7 @@ import { fixedDate } from "../../lib/words"
 
 export const DayCounter = () => {
 	const now = fixedDate(new Date());
-	const start = Date.UTC(2022, 2, 2, 0) - 9 * 60 * 60 * 1000;
+	const start = fixedDate(new Date(2022, 2, 2, 0));
 
 	const day = Math.floor((now - start) / (1000 * 60 * 60 * 24));
 
